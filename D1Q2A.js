e90f5a21e1e5e2c4d98ee48f2c02490e634c8450
amrendrakind <vision2010@gmail.com>
2021-08-28 19:28:39 +0530

    Day1Q2A Done

---
+++ D1Q2A.js
@@ -0,0 +1,13 @@
+
+//Q2.A--------------------------
+
+for(let i=0;i<7;i++)
+    {
+    let str="#"
+     for(let j=0;j<i;j++)
+        {
+            str+="#"
+        }   
+    console.log(str)
+    str=""
+    }
